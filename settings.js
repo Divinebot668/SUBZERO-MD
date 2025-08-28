ba0fdcec883525a9692a1506f1563740e2f3fcd6
Divinebot668 <ebohdivine56@gmail.com>
2025-08-28 07:10:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO~38740826a3290b8a34560779de0f9798e79a6816", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2347015996525", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
